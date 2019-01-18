0560e325e6d6244096a2dce0c5196cf591bb0907
A. Z. Guo <albertguotrs@hotmail.com>
2019-01-18 04:47:47 -0600

    comment improvement

--- ID3.py
+++ ID3.py
@@ -158,7 +158,7 @@ def H(examples, attribute, attribute_value):
 def entropy_gain(examples, attribute):
     values = []
     for example in examples:
-        # if an attribute value is missing, replace '?' with the mode value
+        # if an attribute value is missing, replace '?' with the attribute's mode value
         if example[attribute] == '?':
             values.append(mode_attribute_value(examples, attribute))
         else:
