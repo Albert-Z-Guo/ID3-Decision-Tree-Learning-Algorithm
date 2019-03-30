9a82049c37b40622ee7ded4709f72fab6c9934d0
A. Z. Guo <albertguotrs@hotmail.com>
2019-03-30 14:21:25 -0500

    structure improvement

--- parse.py
+++ parse.py
@@ -1,18 +1,18 @@
 import csv
 
 def parse(filename):
-  '''
-  takes a filename and returns attribute information and all the data in array of dictionaries
-  '''
-  # initialize variables
-  out = []
-  csvfile = open(filename,'r')
-  fileToRead = csv.reader(csvfile)
+    '''
+    takes a filename and returns attribute information and all the data in array of dictionaries
+    '''
+    data = []
 
-  headers = next(fileToRead)
+    with open(filename, 'r') as file:
+        csv_file = csv.reader(file)
+        # skip the first row
+        headers = next(csv_file)
 
-  # iterate through rows of actual data
-  for row in fileToRead:
-    out.append(dict(zip(headers, row)))
+        # iterate through rows of actual data
+        for row in csv_file:
+            data.append(dict(zip(headers, row)))
 
-  return out
+    return data
